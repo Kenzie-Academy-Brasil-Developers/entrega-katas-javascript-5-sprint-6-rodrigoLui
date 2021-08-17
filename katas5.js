981f30dd5219024ef4104caeb8f5f0af7616fd07
Rodrigo Luiz <79553015+rodrigoLui@users.noreply.github.com>
2021-08-17 10:39:19 -0300

    testando

--- katas5.js
+++ katas5.js
@@ -1,1 +1,12 @@
 // implemente aqui as funções de teste
+function testeReverseString1() {
+    let result = reverseString("Kenzie Academy");
+    let expected = "ymedacA eizneK";
+    console.assert(result === expected, `esperado: ${expected}, obtido: ${result}`)
+}
+
+function testeReverseString2() {
+    let result = reverseString("Kenzie Academy");
+    let expected = "Academy Kenzie";
+    console.assert(result === expected, `esperado: ${expected}, obtido: ${result}`)
+}
