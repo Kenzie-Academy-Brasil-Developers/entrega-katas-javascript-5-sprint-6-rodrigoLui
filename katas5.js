9fb15d85fe72792af49940ecc81d0981f74fc7c0
Rodrigo Luiz <79553015+rodrigoLui@users.noreply.github.com>
2021-08-17 20:37:13 -0300

    metade ja feito, falta os katas do 6 em diante

--- katas5.js
+++ katas5.js
@@ -1,14 +1,21 @@
 // implemente aqui as funções de teste
+
+
+// KATA 01
 function testeReverseString1() {
     let result = reverseString("Kenzie Academy");
     let expected = "ymedacA eizneK";
     console.assert(result === expected, `esperado: ${expected}, obtido: ${result}`)
 }
+function testeReverseString2() {
+    let result = reverseString(`Kenzie${2} ${4}Academy`);
+    let expected = "ymedacA4 2eizneK";
+    console.assert(result === expected, `esperado: ${expected}, obtido: ${result}`)
+}
 
 function reverseString (str) {
-    let strArray = new Array
+    let strArray = str.split(' ')
     let reverseStr = new Array
-    strArray = str.split(' ')
 
     for (let i = 0; i < strArray.length; i++) {
         str = strArray[i].split('')
@@ -19,5 +26,119 @@ function reverseString (str) {
     reverseStr.reverse()
     return reverseStr.join(' ')
 } 
-
 testeReverseString1()
+testeReverseString2()
+
+// KATA 02
+const testReverseSentence1 = () => {
+    let result = reverseSentence("bob likes dogs")
+    let expected = "dogs likes bob"
+    console.assert(result === expected, `esperado: ${expected}, obtido: ${result}`)
+}
+const testReverseSentence2 = () => {
+    let result = reverseSentence(`bob - likes ${2} dogs #`)
+    let expected =  "# dogs 2 likes - bob"
+    console.assert(result === expected, `esperado: ${expected}, obtido: ${result}`)
+}
+
+const reverseSentence = (str) => {
+    let strArray = str.split(' ')
+    strArray.reverse()
+    return strArray.join(' ')
+}
+testReverseSentence1 ()
+testReverseSentence2 ()
+
+// KATA 03
+const testMinimumValue1 = (num) => {
+    let result = minimumValue([4,1,2,8,4,3])
+    let expected =  1
+    console.assert(result === expected, `esperado: ${expected}, obtido: ${result}`)
+}
+const testMinimumValue2 = (num) => {
+    let result = minimumValue([45,20,"h",8,40,33])
+    let expected =  8
+    console.assert(result === expected, `esperado: ${expected}, obtido: ${result}`)
+}
+
+const minimumValue = num => {
+    let minValue = num[0]
+    for (let i = 0; i < num.length; i++) {
+        if (minValue > num[i]) {
+            minValue = num[i]
+        }
+    }
+    return minValue
+}
+testMinimumValue1()
+testMinimumValue2()
+
+// KATA 04
+const testMaximumValue1 = () => {
+    let result = maximumValue([90,67,50,130,324,9,1,73])
+    let expected = 324
+    console.assert(result === expected, `esperado: ${expected}, obtido: ${result}`)
+}
+const testMaximumValue2 = () => {
+    let result = maximumValue([90,67,50,130,'T',324,9,1,73])
+    let expected = 324
+    console.assert(result === expected, `esperado: ${expected}, obtido: ${result}`)
+}
+
+const maximumValue = num => {
+    let maxValue = num[0]
+    for (let i = 0; i < num.length; i++) {
+        if (maxValue < num[i]) {
+            maxValue = num[i]
+        }        
+    }
+    return maxValue
+}
+testMaximumValue1()
+testMaximumValue2()
+
+// KATA 05
+const testCalculateRemainder1 = () => {
+    let result = calculateRemainder(8, 2)
+    let expected = 0
+    console.assert(result === expected, `esperado: ${expected}, obtido: ${result}`)
+}
+const testCalculateRemainder2 = () => {
+    let result = calculateRemainder(3, 2)
+    let expected = 1
+    console.assert(result === expected, `esperado: ${expected}, obtido: ${result}`)
+}
+
+const calculateRemainder = (numberDivided, numberDivider) => { 
+    return numberDivided % numberDivider 
+}
+testCalculateRemainder1()
+testCalculateRemainder2()
+
+// KATA 06
+
+const testDistinctValues1 = () => {
+    let result = distinctValues([2,2,1,4,1,3,8,8,3])
+    let expected = [1,2,3,4,8]
+    console.assert(result === expected, `esperado: ${expected}, obtido: ${result}`)
+}
+const testDistinctValues2 = () => {
+    let result = distinctValues([2,2,1,4,1,8,8,9,1,1,2])
+    let expected = [1,2,4,8,9]
+    console.assert(result === expected, `esperado: ${expected}, obtido: ${result}`)
+}
+
+const distinctValues = numbers => {
+    let arrayDistinct = new Array
+
+    for (let i = 0; i < numbers.length; i++) {
+        
+        let aux = numbers[i]
+        
+        arrayDistinct.push(aux)
+    }
+
+    return arrayDistinct
+} 
+testDistinctValues1 ()
+testDistinctValues2 ()
